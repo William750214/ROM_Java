7a77c63d1b5e6252b04a12368a9c47e2f8e397b4
wangmin <wangmin@cnshippingnt.com>
2018-08-22 21:12:29 +0800

    test

--- chap12/src/com/heynas/test/TestEnumMap.java
+++ chap12/src/com/heynas/test/TestEnumMap.java
@@ -13,15 +13,15 @@ import java.util.Map;
  * @version 1.0
  */
 public class TestEnumMap {
-	public static void main(String[] args) {
-		Map map01 = new EnumMap(Month.class);
+    public static void main(String[] args) {
+        Map map01 = new EnumMap(Month.class);
 
-		map01.put(Month.JANUARY,"1月");
-		map01.put(Month.FEBRUARY,"2月");
-		map01.put(Month.MARCH,"3月");
+        map01.put(Month.JANUARY, "1月");
+        map01.put(Month.FEBRUARY, "2月");
+        map01.put(Month.MARCH, "3月");
 
-		System.out.println(map01);
+        System.out.println(map01);
 
-		System.out.println(map01.get(Month.FEBRUARY));
-	}
+        System.out.println(map01.get(Month.FEBRUARY));
+    }
 }
